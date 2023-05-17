5bd47dbd5cbd4bb3a0ce8725a649d154b694c1d6
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-05-17 19:54:39 +0530

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Main } from "./pages/main";
 import { Login } from "./login";
+import { Navbar } from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
